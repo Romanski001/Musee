62b4879a50f8ba48510bfeaa8f681948cd19d31a
Romain Maciejewski <romain.maciejewski@laposte.net>
2017-02-21 15:47:46 +0100

    lancement du site

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'galleries#index'
+  resources :galleries, only:[:index, :show]
+
+  #get'galleries', 'galleries#index', as:'galleries'
+  #get'galleries/:id',
+
   namespace :admin do
     resources :galleries
   end
